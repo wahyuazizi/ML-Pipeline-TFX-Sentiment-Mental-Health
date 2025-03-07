54f8647bc6bec3237993ab50f40cc0c021a2d4df
Azizi <wahyuazizi03@gmail.com>
2025-03-08 01:46:36 +0800

    update

--- data_transform.py
+++ data_transform.py
@@ -2,8 +2,8 @@
 import tensorflow as tf
 import tensorflow_transform as tft
 
-LABEL_KEY = 'status'
-FEATURE_KEY = 'statement'
+LABEL_KEY = 'label'
+FEATURE_KEY = 'text'
 def transformed_name(key):
     return key + "_xf"
 
@@ -12,9 +12,7 @@ def preprocessing_fn(inputs):
     outputs = {}
     # Pastikan teks dalam bentuk DenseTensor sebelum transformasi
     text_input = inputs[FEATURE_KEY]
-    # if isinstance(text_input, tf.SparseTensor):
-    #     text_input = tf.sparse.to_dense(text_input, default_value="")
-
+    
     outputs[transformed_name(FEATURE_KEY)] = tf.strings.lower(text_input)
     outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
     return outputs
---
+++ mental_health_trainer.py
@@ -0,0 +1,7 @@
+
+import tensorflow as tf
+import tensorflow_transform as tft
+from tensorflow.keras import layers
+import os
+import tensorflow_hub as hub
+from tfx.components.trainer.fn_args_utils import FnArgs
